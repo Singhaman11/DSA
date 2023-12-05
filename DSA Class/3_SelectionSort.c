3896ea78d2e1070c7f5a90c691d4b7f8abce1c08
Singhaman11 <amansingh091104@outlook.com>
2023-12-05 05:45:14 +0530

    Complete

--- DSA Class/3_SelectionSort.c
+++ DSA Class/3_SelectionSort.c
@@ -3,7 +3,7 @@
 int main()
 {
     int ar[] = {5, 6, 9, 7, 8};
-    int max = 0, pos = 0, f = 0;
+    int temp = 0, pos = 0, f = 0;
     int x = 0;
     int n = sizeof(ar) / sizeof(int);
     printf("Unsorted array\n");
@@ -23,9 +23,9 @@ int main()
                 // x++;
             }
         }
-        max = ar[i];
+        temp = ar[i];
         ar[i] = ar[pos];
-        ar[pos] = max;
+        ar[pos] = temp;
         // if(f == 0)
             // break;
         // f = 0;
