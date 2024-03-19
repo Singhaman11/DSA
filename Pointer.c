76b215e74b852628cf02d3f9d2598bcab4424cbc
Singhaman11 <amansingh091104@outlook.com>
2024-03-19 15:05:24 +0530

    Fix

--- Pointer.c
+++ Pointer.c
@@ -3,13 +3,13 @@ void main()
 {
     int x = 10;
     int *y = &x;
-    int *z = &y;
+    // int *z = &y;
     printf("x = %d\n", x);
-    printf("Address x = %d\n", &x);
-    printf("Address x = %p\n", x);
-    printf("y = %d\n", y);
+    // printf("Address x = %d\n", &x);
+    printf("Address x = %p\n", &x);
+    // printf("y = %d\n", y);
     printf("*y = %d\n", *y);
     printf("y = %p\n", y);
-    printf("z = %d\n", z);
-    printf("**z = %d\n", *z);
+    // printf("z = %d\n", z);
+    // printf("**z = %d\n", *z);
 }
